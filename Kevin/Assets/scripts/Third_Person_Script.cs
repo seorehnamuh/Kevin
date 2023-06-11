12e7c05164c02ffa7f991c9c952684603df207a3
Andre-ska <andrea.scala@istitutopantheon.it>
2023-06-11 21:04:53 +0200

    Kevin Camera parte 2

--- Kevin/Assets/scripts/Third_Person_Script.cs
+++ Kevin/Assets/scripts/Third_Person_Script.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Third_Person_Script : MonoBehaviour
 {
     public CharacterController Controlller;
-
-
+    public Transform Cam;
     public float Speed = 6f;
+
+    public float TurnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
     // Update is called once per frame
     void Update()
@@ -18,11 +20,12 @@ public class Third_Person_Script : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + Cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, TurnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-
-            Controlller.Move(direction * Speed * Time.deltaTime);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Controlller.Move(moveDir.normalized * Speed * Time.deltaTime);
         }
     }
 }
