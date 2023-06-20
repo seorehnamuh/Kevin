9500f160796ab053e423cbe54e96b4200dd38faa
seorehnamuh <deborah.fazzuoli@istitutopantheon.it>
2023-06-20 21:33:43 +0200

    added script next level

---
+++ Kevin/Assets/scripts/NextLevelText.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NextLevelText : MonoBehaviour
+{
+    public TextMeshProUGUI nextLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+   
+    IEnumerator ShowMessage(string message, float delay)
+    {
+        nextLevel.text = message;
+        nextLevel.enabled = true;
+        yield return new WaitForSeconds(delay);
+        nextLevel.text = "";
+        nextLevel.enabled = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Test"); 
+            StartCoroutine(ShowMessage("You have been caught, you will be transported in the maze", 5));
+        }
+    }
+}
